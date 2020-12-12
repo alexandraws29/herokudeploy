4b9746cb4764d1a66d69a4635200d4fe89335ae6
Alexa <alexandraws29@outlook.com>
2020-12-12 13:04:44 -0700

    attempting update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const PORT = process.env.PORT || 3001
 const app = express()
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send("Updating things")
 }) 
 
 app.listen(PORT, () => {
